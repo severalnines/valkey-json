c49e529429798a9d67385665f0a5b5b2846e2149
michal.o <michal.o@severalnines.com>
2025-06-07 08:45:56 +0200

    Give venv access to system site-packages

--- build.sh
+++ build.sh
@@ -49,7 +49,7 @@ REQUIREMENTS_FILE="requirements.txt"
 if command -v uv > /dev/null 2>&1; then
     echo "Using uv to create venv and install packages..."
     if [ ! -d "$SCRIPT_DIR/venv" ]; then
-      uv venv "$SCRIPT_DIR/venv"
+      uv venv "$SCRIPT_DIR/venv" --system-site-packages
     fi
     source "$SCRIPT_DIR/venv/bin/activate"
     uv pip install -r "$SCRIPT_DIR/$REQUIREMENTS_FILE"
